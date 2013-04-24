3506fbb43aac832985cfbc6358935a0e82b1e77b
Patrick.Coin <vidtion@gmail.com>
2013-04-24 14:44:50 +0800

    init

---
+++ iDisk/Tools/ZJTTool.h
@@ -0,0 +1,13 @@
+//
+//  ZJTTool.h
+//  iDisk
+//
+//  Created by Patrick.Coin on 13-4-24.
+//  Copyright (c) 2013年 ZJTSoft Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJTTool : NSObject
+
+@end
---
+++ iDisk/Tools/ZJTTool.m
@@ -0,0 +1,13 @@
+//
+//  ZJTTool.m
+//  iDisk
+//
+//  Created by Patrick.Coin on 13-4-24.
+//  Copyright (c) 2013年 ZJTSoft Inc. All rights reserved.
+//
+
+#import "ZJTTool.h"
+
+@implementation ZJTTool
+
+@end
